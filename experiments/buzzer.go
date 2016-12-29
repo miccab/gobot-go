32e3f920bcf7e63fc6eb3285f818e50b7ee8605e
Michal <michal@world.com>
2016-12-29 18:43:02 +0100

    using tone function

--- experiments/buzzer.go
+++ experiments/buzzer.go
@@ -3,15 +3,15 @@ package experiments
 import (
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot"
-	"time"
 )
 
 func GetBuzzerAndWork(writer gpio.DigitalWriter) (devices []gobot.Device, work func())  {
 	buzzer := gpio.NewBuzzerDriver(writer, "11")
 	work = func() {
-		buzzer.On()
-		time.Sleep(1*time.Second)
-		buzzer.Off()
+		buzzer.Tone(gpio.C5, gpio.Whole)
+		buzzer.Tone(gpio.D5, gpio.Half)
+		buzzer.Tone(gpio.E5, gpio.Quarter)
+		buzzer.Tone(gpio.F5, gpio.Eighth)
 	}
 	devices = []gobot.Device{buzzer}
 	return
