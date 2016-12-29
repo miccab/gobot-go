a40e3dc11f0af655bc78ff7dddc7d1958a7d1d70
Michal <michal@world.com>
2016-12-29 07:21:30 +0100

    led blinker

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot"
+	"time"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+func main() {
+	adaptor := raspi.NewAdaptor()
+	led := gpio.NewLedDriver(adaptor, "12")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{adaptor},
+		[]gobot.Device{led},
+		work,
+	)
+	println("INFO: run using sudo")
+	robot.Start()
+}
